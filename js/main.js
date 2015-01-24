676e411f7f5b25d2a33807af06db604dbc9b1143
Arthur Chen <crh0716@gmail.com>
2015-01-24 15:36:15 +0800

    1. Add full screen mode

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var VIDEO_WIDTH = 320;
-var VIDEO_HEIGHT = 240;
+var VIDEO_WIDTH = 1280;
+var VIDEO_HEIGHT = 720;
 
 function getSources(type) {
   if (typeof MediaStreamTrack.getSources === 'undefined') {
@@ -40,22 +40,46 @@ function requestVideo(id) {
   });
 }
 
-var errorMessage = function(event) {
-  var message = 'getUserMedia successful but ' + event.type + ' event fired ' +
-                'from camera. Most likely too many cameras on the same USB ' +
-                'bus/hub. Verify this by disconnecting one of the cameras ' +
-                'and try again.';
-  document.getElementById('messages').innerHTML += event.target.label + ': ' +
-                                                   message + '<br><br>';
-};
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && 
+      !document.webkitFullscreenElement && 
+      !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement
+      .webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
 
 window.onload = function() {
+  document.addEventListener('click', function() {
+    toggleFullScreen();
+  });
+  
   getSources('video').then(function(sources) {
     return Promise.all(sources.map(function(source) {
       return requestVideo(source.id);
     }));
   }).then(function(videos) {
     var stage = document.querySelector('.stage');
+    var defaultVideo = videos[0];
     var v1 = new VideoContainer(videos[0], {
       pos: 'left'
     });
@@ -63,9 +87,61 @@ window.onload = function() {
       pos: 'right',
       mirror: true
     });
+    var v2 = new VideoContainer(videos[1] || defaultVideo, {
+      pos: 'right'
+    });
+    var v2p = new VideoContainer(videos[1] || defaultVideo, {
+      pos: 'left',
+      mirror: true
+    });
+    var v3 = new VideoContainer(videos[2] || defaultVideo, {
+      pos: 'left'
+    });
+    var v4 = new VideoContainer(videos[3] || defaultVideo, {
+      pos: 'right'
+    });
 
     stage.appendChild(v1.root);
     stage.appendChild(v1p.root);
+    stage.appendChild(v2.root);
+    stage.appendChild(v2p.root);
+    stage.appendChild(v3.root);
+    stage.appendChild(v4.root);
+
+    var counter = 0;
+    var steps = [
+      function() {
+        v1.show();
+        v1p.show();
+        v2.hide();
+        v2p.hide();
+        v3.hide();
+        v4.hide();
+      },
+      function() {
+        v2.show();
+        v2p.show();
+        v1.hide();
+        v1p.hide();
+        v3.hide();
+        v4.hide();
+      },
+      function() {
+        v3.show();
+        v4.show();
+        v1.hide();
+        v1p.hide();
+        v2.hide();
+        v2p.hide();
+      }
+    ];
+
+    setInterval(function() {
+      steps[counter++]();
+      if (counter === steps.length) {
+        counter = 0;
+      }
+    }, 5000);
   }).catch(function(error) {
     console.error(error);
   });
--- js/video_container.js
+++ js/video_container.js
@@ -1,21 +1,20 @@
 'use strict';
 
-var WIDTH = 320;
-var HEIGHT = 240;
-
 function VideoContainer(stream, options) {
   this._root = document.createElement('div');
   this._root.className = 'video-container';
 
   this._video = document.createElement('video');
-  this._video.width = WIDTH;
-  this._video.height = HEIGHT;
   this._video.autoplay = true;
 
-  this.setPos((options && options.pos) || 'left');
+  this.setPosition((options && options.pos) || 'left');
   this.setMirror((options && options.mirror) || false);
+  if (options && options.visible) {
+    this.show();
+  }
+  this.setSize(1024, 768);
 
-  this._root.appendChild(video);
+  this._root.appendChild(this._video);
   this.setStream(stream);
 }
 
@@ -30,7 +29,12 @@ VideoContainer.prototype.setStream = function(stream) {
 };
 
 VideoContainer.prototype.setPosition = function(pos) {
-  this._root.dataset = pos;
+  this._root.dataset.pos = pos;
+};
+
+VideoContainer.prototype.setSize = function(width, height) {
+  this._video.width = width;
+  this._video.height = height;
 };
 
 VideoContainer.prototype.setMirror = function(mirror) {
